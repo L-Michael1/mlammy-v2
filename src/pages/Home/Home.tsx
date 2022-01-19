809668e8f26c20663bff5515c0171fc80599e82e
Michael Lam <mlam11@uoguelph.ca>
2022-01-19 00:17:22 -0500

    clean up

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,41 +4,6 @@ const Home = ({ themeType, toggleTheme }: any) => {
     return (
         <>
             <Navbar themeType={themeType} toggleTheme={toggleTheme} />
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-
         </>
     )
 }
